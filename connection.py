33ad6e8229a606375d21145b01788228b3d41c28
Will Gallia <w.gallia@arts.ac.uk>
2018-03-07 17:07:15 +0000

    going back and forth

--- connection.py
+++ connection.py
@@ -1,6 +1,10 @@
 from serial import Serial
+from serial.serialutil import SerialException
 from serial.tools import list_ports
 import struct
+import threading
+import time
+import re
 
 class Connection:
     _serial = None
@@ -11,36 +15,125 @@ class Connection:
     BANG = b'\xf2'
     STRING = b'\xf3'
 
-    def __init__(self, serial):
-        self.serial = serial
+    # Singleton pattern
+    _instance = None
+    def __new__(cls):
+        if Connection._instance is None:
+            Connection._instance = object.__new__(cls)
+        return Connection._instance
 
-    @staticmethod
-    def auto(baud=115200):
-        serial = Connection._serial
-        print('serial is', serial)
-        if not serial:
-            for port in list_ports.comports():
-                if 'Arduino' in port.description:
-                    serial = Serial(port.device, baud)
-                    print('found port {} {}'.format(port.device, port.description))
-                    break
+    def __init__(self):
+        self.wss = {}
+        self._connect()
+
+    def _connect(self, baud=115200):
+        serial = None
+        for port in list_ports.comports():
+            if 'Arduino' in port.description:
+                serial = Serial(port.device, baud)
+                print('found port {} {}'.format(port.device, port.description))
+                break
 
         if not serial:
             print('no serial found')
             pass
         else:
             Connection._serial = serial
-            return Connection(serial)
+            self.serial = serial
 
     @staticmethod
     def close():
         if Connection._serial:
             Connection._serial.close()
 
+    def register(self, name, ws):
+        self.wss[name] = ws
+        print('registered', name, 'to', ws)
+
+    def unregister(self, name):
+        del self.wss[name]
+
+    def start_listening(self):
+        self.thread = threading.Thread(name='serial-listener',
+                                       target=self._listen_thread)
+        self.listen = threading.Event()
+        self.listen.set()
+        self.thread.start()
+
+    def stop_listening(self):
+        self.serial.cancel_read()
+        self.listen.clear()
+
+    def send_to_arduino(self, o):
+        packet = self._construct_packet(o['key'], o['value'])
+        try:
+            self.serial.write(packet)
+        except (OSError, SerialException):
+            self._connect()
+            time.sleep(1)
+
+    def _listen_thread(self):
+        time.sleep(0.2)
+        print('starting listening')
+        buffer = ''
+        while self.listen.is_set():
+            try:
+                b = self.serial.read()
+                buffer += b.decode('utf8')
+                buffer = self._process_incoming(buffer)
+            except (OSError, SerialException):
+                self._connect()
+                time.sleep(1)
+
+        print('end listening')
+
+    def _process_incoming(self, data):
+        data = re.sub(r'\r|\n', '', data)
+        match = re.match(r'\|(\w+),(b|i|f),([\w\.]*)\|', data)
+        
+        if match:
+            key, t, d = match.groups()
+
+            if t == 'f':
+                d = float(d)
+            elif t == 'i':
+                d = int(d)
+
+            o = { 'key': key, 'type': t }
+
+            if t != 'b':
+                o['value'] = d
+
+            for _, ws in self.wss.items():
+                ws.send(o)
+                print('sending', o)
+
+            _, end = match.span()
+            return self._process_incoming(data[end:])
+
+        match = re.match(r'{([^{}]+)}', data)
+        if match:
+            message = match.groups()[0]
+            print('message:', message)
+            _, end = match.span()
+            return self._process_incoming(data[end:])
+
+        return data
+
     def _construct_packet(self, key, data):
         o = Connection.STX
         o += bytes([len(key)])
         o += key.encode('utf8')
+
+        # try parsing an int and float
+        if type(data) == str:
+            try:
+                data = int(data)
+            except ValueError:
+                try:
+                    data = float(data)
+                except ValueError:
+                    pass
 
         if type(data) == int:
             o += Connection.INT
--- mediator.py
+++ mediator.py
@@ -3,28 +3,40 @@ from collections import OrderedDict
 from connection import Connection
 import json
 
-class Mediator(WebSocketApplication):
+class WebSocketReceiver(WebSocketApplication):
     def on_open(self):
-        print("Connection opened", self.ws.environ)
-        self.conn = Connection.auto()
+        print("Connection opened")
+        self.name = self.ws.environ['REMOTE_PORT']
+        self.conn = Connection()
+        self.conn.register(self.name, self)
+        print('registering')
 
     def on_message(self, message):
         if not self.ws.closed:
             o = json.loads(message)
-            print(o)
-           
+            self.conn.send_to_arduino(o)
+            print('received {}: {}'.format(o['key'], o['value']))
+
+    def send(self, data):
+        self.ws.send(json.dumps(data))
 
     def on_close(self, reason):
         print("reason is", reason)
+        self.conn.unregister(self.name)
 
 if __name__ == "__main__":
-    wss = WebSocketServer(('', 5000),
-        Resource(OrderedDict([('/', Mediator)])))
+    wss = WebSocketServer(('', 7776),
+        Resource(OrderedDict([('/', WebSocketReceiver)])))
 
-    if not Connection.auto():
+    conn = Connection()
+
+    if not conn:
         print('error no serial port found')
+    else:
+        conn.start_listening()
 
     try:
         wss.serve_forever()
     except KeyboardInterrupt:
+        conn.stop_listening()
         print('bye')
